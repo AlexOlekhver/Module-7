0eb3f4876d3c85215b3d7d5f2b2cfb3bc5c0b84b
Leshich <leshicher@mail.ru>
2022-05-16 22:12:11 +0300

    add 7.1.5

---
+++ 7.1.5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _7._1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Food
+    {
+
+    }
+    
+    class Fruits: Food
+    {
+
+    }
+    class Vegetables: Food
+    {
+
+    }
+    class Apple: Fruits
+    {
+
+    }
+    class Banana: Fruits
+    {
+
+    }
+    class Pear: Fruits
+    {
+
+    }
+    class Potato: Vegetables
+    {
+
+    }
+    class Carrot: Vegetables
+    {
+
+    }
+}
