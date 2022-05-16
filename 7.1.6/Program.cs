eb657e011496fb287de5a627c20156a20d20020a
Leshich <leshicher@mail.ru>
2022-05-16 22:26:37 +0300

    Add 7.1.6.

---
+++ 7.1.6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _7._1._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Obj
+    {
+        private string name;
+        private string owner;
+        private int length;
+        private int count;
+
+        public Obj(string name, string ownerName, int objLength, int count)
+        {
+            // ...
+        }
+    }
+}
