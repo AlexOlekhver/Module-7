7113a02bdf426262084a13fedcf0b7ee0cb9c9d0
Leshich <leshicher@mail.ru>
2022-05-17 22:57:21 +0300

    add 7.2.14

---
+++ 7.2.14/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _7._2._14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class IndexingClass
+    {
+        private int[] array;
+
+        public IndexingClass(int[] array)
+        {
+            this.array = array;
+        }
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value;
+            }
+        } 
+    }
+}
