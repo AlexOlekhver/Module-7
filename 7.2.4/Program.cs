1a1ea463ca36d993d45025ec24883520ffb4094e
Leshich <leshicher@mail.ru>
2022-05-17 06:29:07 +0300

    add 7.2.4

---
+++ 7.2.4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _7._2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class BaseClass
+    {
+        public virtual int Counter
+        {
+            get;
+            set;
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        private int counter;
+        public override int Counter
+        {
+            get
+            {
+                return counter;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    counter = value;
+                }
+            }
+        }
+    }
+}
