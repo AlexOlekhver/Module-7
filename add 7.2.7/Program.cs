6efaa0a56f663ed77fe9a39f331f0c6637ee437a
Leshich <leshicher@mail.ru>
2022-05-17 06:49:44 +0300

    add 7.2.7

---
+++ add 7.2.7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace add_7._2._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class A
+    {
+        public virtual void Display()
+        {
+            Console.WriteLine("A");
+        }
+    }
+    class B: A
+    {
+        public new void Display()
+        {
+            Console.WriteLine("B");
+        }
+    }
+    class C: A
+    {
+        public override void Display()
+        {
+            Console.WriteLine("C");
+        }
+    }
+    class D: B
+    {
+        public new void Display()
+        {
+            Console.WriteLine("D");
+        }
+    }
+    class E: C
+    {
+        public new void Display()
+        {
+            Console.WriteLine("E");
+        }
+    }
+}
