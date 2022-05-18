1f022ae62188aa7953fb4d739521c935542ab11a
Leshich <leshicher@mail.ru>
2022-05-18 06:23:17 +0300

    Add 7.3.3.

---
+++ Add 7.3.3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Add_7._3._3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    abstract class ComputerPart
+    {
+        public abstract void Work();
+    }
+    class MotherBoard: ComputerPart
+    {
+        public override void Work()
+        {
+            Console.WriteLine("Mother");
+        }
+    }
+    class Processor: ComputerPart
+    {
+        public override void Work()
+        {
+            
+        }
+    }
+}
