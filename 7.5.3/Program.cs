45edb32cc4d402125aec34348657b1cc4a0f22b0
Leshich <leshicher@mail.ru>
2022-05-19 22:44:00 +0300

    add 7.5.3

---
+++ 7.5.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _7._5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 3;
+            int num2 = 58;
+
+            Helper.Swap(ref num1, ref num2);
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+        }
+    }
+    class Helper
+    {
+        public static void Swap(ref int a,ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+
+        }
+    }
+}
