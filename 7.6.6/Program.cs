63bfcda329dea1b932bf1437a9821d30a3d5a95e
Leshich <leshicher@mail.ru>
2022-05-20 23:04:41 +0300

    add 7.6.6

---
+++ 7.6.6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _7._6._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Record <T1, T2>
+    {
+        public T1 ld;
+        public T2 Value;
+        public DateTime Date;
+    }
+}
